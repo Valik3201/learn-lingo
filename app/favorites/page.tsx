fe2afc49a78817fc9149f3cbd3d225431f2d1efa
Valik <valik3201@gmail.com>
2024-07-14 16:59:03 +0200

    feat(pages): Major update for pages teachers and favorites

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
 import { useFavoritesStore } from "@/store/useFavoritesStore";
-import { usePaginatedData } from "@/hooks/use-paginated-data";
-import type { Teacher } from "@/store/useTeachersStore";
-import { TeacherCard } from "@/components/teacher-card";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectGroup,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { TeacherPage } from "@/components/teachers-page";
 
 export default function Page() {
   const {
@@ -23,99 +10,11 @@ export default function Page() {
     loading: loadingFavorites,
   } = useFavoritesStore();
 
-  const {
-    paginatedData: favorites,
-    loadMore,
-    hasMore,
-  } = usePaginatedData<Teacher>(favoritesStore, 5);
-
-  useEffect(() => {
-    fetchFavorites();
-  }, [fetchFavorites]);
-
   return (
-    <main className="md:px-16 space-y-8">
-      <style jsx global>{`
-        body {
-          background-color: #f8f8f8 !important;
-        }
-      `}</style>
-
-      <div className="flex flex-col md:flex-row gap-4">
-        <div className="space-y-2">
-          <Label className="text-[#8A8A89]">Languages</Label>
-          <Select>
-            <SelectTrigger className="w-[221px]">
-              <SelectValue placeholder="Select language" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="fr">French</SelectItem>
-                <SelectItem value="en">English</SelectItem>
-                <SelectItem value="de">German</SelectItem>
-                <SelectItem value="ua">Ukrainian</SelectItem>
-                <SelectItem value="pl">Polish</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-
-        <div className="space-y-2">
-          <Label className="text-[#8A8A89]">Level of knowledge</Label>
-          <Select>
-            <SelectTrigger className="w-[221px]">
-              <SelectValue placeholder="Select level" />
-            </SelectTrigger>
-            <SelectContent className="w-[221px]">
-              <SelectGroup>
-                <SelectItem value="a1">A1 Beginner</SelectItem>
-                <SelectItem value="a2">A2 Elementary</SelectItem>
-                <SelectItem value="b1">B1 Intermediate</SelectItem>
-                <SelectItem value="b2">B2 Upper-Intermediate</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-
-        <div className="space-y-2">
-          <Label className="text-[#8A8A89]">Price</Label>
-          <Select>
-            <SelectTrigger className="w-[221px] md:w-[124px]">
-              <SelectValue placeholder="Select price" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="10">10 $</SelectItem>
-                <SelectItem value="20">20 $</SelectItem>
-                <SelectItem value="30">30 $</SelectItem>
-                <SelectItem value="40">40 $</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-
-      {loadingFavorites && <p className="text-right">Loading...</p>}
-
-      <ul className="flex flex-col gap-8">
-        {favorites.map((teacher) => (
-          <li key={teacher.id}>
-            <TeacherCard teacher={teacher} />
-          </li>
-        ))}
-      </ul>
-
-      {hasMore && (
-        <div className="text-center py-8">
-          <Button
-            variant={"yellow"}
-            onClick={loadMore}
-            className="text-lg px-12 py-6 w-full sm:w-fit"
-          >
-            Load More
-          </Button>
-        </div>
-      )}
-    </main>
+    <TeacherPage
+      fetchTeachers={fetchFavorites}
+      loadingTeachers={loadingFavorites}
+      store={favoritesStore}
+    />
   );
 }
--- app/teachers/page.tsx
+++ app/teachers/page.tsx
@@ -1,22 +1,10 @@
 "use client";
 
 import { useEffect } from "react";
-import type { Teacher } from "@/store/useTeachersStore";
 import { useAuthStore } from "@/store/useAuthStore";
 import { useTeachersStore } from "@/store/useTeachersStore";
 import { useFavoritesStore } from "@/store/useFavoritesStore";
-import { usePaginatedData } from "@/hooks/use-paginated-data";
-import { TeacherCard } from "@/components/teacher-card";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectGroup,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { TeacherPage } from "@/components/teachers-page";
 
 export default function Page() {
   const {
@@ -24,14 +12,9 @@ export default function Page() {
     loading: loadingTeachers,
     fetchTeachers,
   } = useTeachersStore();
-  const { fetchFavorites, loading: loadingFavorites } = useFavoritesStore();
-  const { user, loading: loadingAuth } = useAuthStore();
 
-  const {
-    paginatedData: teachers,
-    loadMore,
-    hasMore,
-  } = usePaginatedData<Teacher>(teachersStore, 5);
+  const { fetchFavorites, loading: loadingFavorites } = useFavoritesStore();
+  const { user } = useAuthStore();
 
   useEffect(() => {
     fetchTeachers();
@@ -41,90 +24,10 @@ export default function Page() {
   }, [fetchTeachers, fetchFavorites, user]);
 
   return (
-    <main className="md:px-16 space-y-8">
-      <style jsx global>{`
-        body {
-          background-color: #f8f8f8 !important;
-        }
-      `}</style>
-
-      <div className="flex flex-col md:flex-row gap-4">
-        <div className="space-y-2">
-          <Label className="text-[#8A8A89]">Languages</Label>
-          <Select>
-            <SelectTrigger className="w-[221px]">
-              <SelectValue placeholder="Select language" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="fr">French</SelectItem>
-                <SelectItem value="en">English</SelectItem>
-                <SelectItem value="de">German</SelectItem>
-                <SelectItem value="ua">Ukrainian</SelectItem>
-                <SelectItem value="pl">Polish</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-
-        <div className="space-y-2">
-          <Label className="text-[#8A8A89]">Level of knowledge</Label>
-          <Select>
-            <SelectTrigger className="w-[221px]">
-              <SelectValue placeholder="Select level" />
-            </SelectTrigger>
-            <SelectContent className="w-[221px]">
-              <SelectGroup>
-                <SelectItem value="a1">A1 Beginner</SelectItem>
-                <SelectItem value="a2">A2 Elementary</SelectItem>
-                <SelectItem value="b1">B1 Intermediate</SelectItem>
-                <SelectItem value="b2">B2 Upper-Intermediate</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-
-        <div className="space-y-2">
-          <Label className="text-[#8A8A89]">Price</Label>
-          <Select>
-            <SelectTrigger className="w-[221px] md:w-[124px]">
-              <SelectValue placeholder="Select price" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="10">10 $</SelectItem>
-                <SelectItem value="20">20 $</SelectItem>
-                <SelectItem value="30">30 $</SelectItem>
-                <SelectItem value="40">40 $</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-
-      {(loadingTeachers || loadingFavorites || loadingAuth) && (
-        <p className="text-right">Loading...</p>
-      )}
-
-      <ul className="flex flex-col gap-8">
-        {teachers.map((teacher) => (
-          <li key={teacher.id}>
-            <TeacherCard teacher={teacher} />
-          </li>
-        ))}
-      </ul>
-
-      {hasMore && (
-        <div className="text-center py-8">
-          <Button
-            variant={"yellow"}
-            onClick={loadMore}
-            className="text-lg px-12 py-6"
-          >
-            Load More
-          </Button>
-        </div>
-      )}
-    </main>
+    <TeacherPage
+      fetchTeachers={fetchTeachers}
+      loadingTeachers={loadingTeachers || loadingFavorites}
+      store={teachersStore}
+    />
   );
 }
