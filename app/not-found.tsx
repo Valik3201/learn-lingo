b467a999939f9beeb62a04738ff1b0af6036d9d4
Valik <valik3201@gmail.com>
2024-07-23 14:30:59 +0200

    fix: Escape single quotes in HTML using `&apos;` to resolve errors

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
               404 Not Found
             </h1>
             <p className="mb-4 text-2xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
-              Whoops! That page doesn’t exist.
+              Whoops! That page doesn&apos;t exist.
             </p>
             <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
               You'll find lots to explore on the home page.
