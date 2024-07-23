f497b22a084d4723437857e50a47db660a1a97fc
Valik <valik3201@gmail.com>
2024-07-23 14:28:35 +0200

    style: Update footer with links for better navigation

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,30 @@
-import { LogoIcon } from "./icons";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { LogoIcon } from "@/components/icons";
 
 export function Footer() {
   return (
     <footer className="mt-auto flex flex-col md:flex-row gap-4 items-center justify-between md:px-16 py-5">
       <div className="flex items-center gap-2 font-medium">
-        <LogoIcon className="mr-2" />
-        LearnLingo
+        <Link href="/" className="flex items-center gap-2 font-medium w-fit">
+          <LogoIcon className="mr-2" />
+          LearnLingo
+        </Link>
       </div>
-      <div>© {new Date().getFullYear()} LearnLingo. All rights reserved.</div>
+      <div>
+        © {new Date().getFullYear()}{" "}
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "p-0 text-base font-normal"
+          )}
+        >
+          LearnLingo
+        </Link>
+        . All rights reserved.
+      </div>
     </footer>
   );
 }
